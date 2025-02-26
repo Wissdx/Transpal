0dd5ab96bc942b5c6606039ad84766f6ebd8a0f1
Thomas Goillot <thomadgllt@gmail.com>
2025-02-27 00:48:26 +0100

    fix(mainActivity) : the name of the user dynamic in the main activity

--- app/src/main/java/fr/esgi/transpal/MainActivity.kt
+++ app/src/main/java/fr/esgi/transpal/MainActivity.kt
@@ -1,20 +1,26 @@
 package fr.esgi.transpal
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val userImage = findViewById<ImageView>(R.id.user_image);
-        val userName = findViewById<TextView>(R.id.user_name);
+        val userImage = findViewById<ImageView>(R.id.user_image)
+        val userName = findViewById<TextView>(R.id.user_name)
+
+        val sharedPreferences = getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+        val userNameString = sharedPreferences.getString("user_name", "Utilisateur")
+
+        userName.text = userNameString
 
         userImage.setOnClickListener {
             val intent = Intent(this, ProfileActivity::class.java)
